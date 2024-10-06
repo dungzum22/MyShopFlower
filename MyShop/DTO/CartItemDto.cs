95244c219bd825ae0a1b89c1b0d2edb772741494
dungzum22 <dung521453@gmail.com>
2024-10-06 09:05:59 +0700

    API cart

---
+++ MyShop/DTO/CartItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShop.DTO
+{
+    public class CartItemDto
+    {
+        public string FlowerName { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ MyShop/DTO/CartSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.DTO
+{
+    public class CartSummaryDto
+    {
+        public List<CartItemDto> Items { get; set; } = new List<CartItemDto>();
+        public int TotalQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
